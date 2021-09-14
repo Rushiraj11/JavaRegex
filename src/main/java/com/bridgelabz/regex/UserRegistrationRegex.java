7214e5f9820701f4031da1b9bc03d5dee4aa2b14
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 20:50:05 +0530

    Modified: Enter valid First Name

--- src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
+++ src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistrationRegex {
     public static void main(String[] args) {
-        String input="Rushiraj";
+        String input="RUSHIRAJ";
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
