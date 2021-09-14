d0aa8c8cf783e8dda6d7bfb210e9c1a6f24db626
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 18:29:45 +0530

    Added : Validation of Invalid case

--- src/main/java/com/bridgelabz/regex/PinCodeRegex.java
+++ src/main/java/com/bridgelabz/regex/PinCodeRegex.java
@@ -5,7 +5,8 @@ public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaRegex program");
 
-        String input = "400088";
+       // String input = "400088";
+        String input = "A400088"; // UC2 validation
         String regex = "^[1-9]{1}[0-9]{5}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
