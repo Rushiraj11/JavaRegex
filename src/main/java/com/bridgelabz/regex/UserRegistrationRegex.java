f74d424827ece552a74e406169bb9e56149ec026
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 22:28:53 +0530

    Added: Enter valid EmailId

--- src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
+++ src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
@@ -5,6 +5,7 @@ public class UserRegistrationRegex {
     public static void main(String[] args) {
         firstNameName();
         lastName();
+        emailId();
     }
         public static void firstNameName(){
             String Firstnameinput = "Rushiraj";
@@ -29,5 +30,19 @@ public class UserRegistrationRegex {
                 System.out.println("Invalid");
             }
         }
+
+        public static void emailId(){
+            String input = "abc.xyz@bl.co.in";
+            String regex = "^[a-c]+([.+-_][0-9a-z]+)*@[bl]{2}+.[co]{2}+.[a-z]{2}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches()) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+
     }
+}
+
 
