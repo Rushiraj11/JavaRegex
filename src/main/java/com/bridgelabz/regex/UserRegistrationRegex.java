6b6258c30679114270e23c90b2f94f6c1ecb5ae0
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 20:47:38 +0530

    Added: Enter valid First Name

--- src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
+++ src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class EmailRegex {
+public class UserRegistrationRegex {
     public static void main(String[] args) {
-
-        String input = "abc.xyz@bridgelabz.co";
-       String regex = "^[a-c]+([.+-_][0-9a-z]+)*@[bridgelabz]{10}+.[co]{2}$"; // : < > / ?
+        String input="Rushiraj";
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         if (matcher.matches()) {
@@ -15,4 +14,5 @@ public class EmailRegex {
         }
 
     }
+
 }
