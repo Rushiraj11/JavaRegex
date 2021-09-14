08f3a62a243aba5328ec0e739b71fd61cb55cac5
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 19:15:04 +0530

    Added: Modification of Input value validation

--- src/main/java/com/bridgelabz/regex/PinCodeRegex.java
+++ src/main/java/com/bridgelabz/regex/PinCodeRegex.java
@@ -5,10 +5,12 @@ public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaRegex program");
 
-       // String input = "400088";
+       // String input = "400088"; //UC1
        // String input = "A400088"; // UC2 validation
-        String input = "400088B"; // UC3 validation
-        String regex = "^[1-9]{1}[0-9]{5}$";
+       // String input = "400088B"; // UC3 validation
+        String input = "400 088";
+        String regex = "^[1-9]{1}[0-9]{2}\\s{0,2}[0-9]{3}$"; // valid with and without space
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         if (matcher.matches()) {
