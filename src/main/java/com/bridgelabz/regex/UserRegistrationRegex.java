672b26ac8e233fd8f49cd032f4dc39e783e1284c
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 21:16:27 +0530

    Added: Enter valid Last Name

--- src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
+++ src/main/java/com/bridgelabz/regex/UserRegistrationRegex.java
@@ -3,16 +3,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistrationRegex {
     public static void main(String[] args) {
-        String input="RUSHIRAJ";
+        firstNameName();
+        lastName();
+    }
+        public static void firstNameName(){
+            String Firstnameinput = "Rushiraj";
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = pattern.matcher(Firstnameinput);
         if (matcher.matches()) {
             System.out.println("Valid");
         } else {
             System.out.println("Invalid");
         }
-
     }
 
-}
+        public static void lastName(){
+            String Lastnameinput = "Ghatge";
+            String regex = "^[A-Z]{1}[a-z]{2,}$";
+            Pattern pattern1 = Pattern.compile(regex);
+            Matcher matcher = pattern1.matcher(Lastnameinput);
+            if (matcher.matches()) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+
