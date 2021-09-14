94e49599d3903f1fde38aad54e04a5209eec4ac6
Rushiraj11 <grushiraj95@gmail.com>
2021-09-14 17:43:25 +0530

    Added: Regex to validate given Pincode input

--- src/main/java/com/bridgelabz/regex/PinCodeRegex.java
+++ src/main/java/com/bridgelabz/regex/PinCodeRegex.java
@@ -1,8 +1,21 @@
 package com.bridgelabz.regex;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaRegex program");
+
+        String input = "400088";
+        String regex = "^[1-9]{1}[0-9]{5}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+
+
     }
 
 }
